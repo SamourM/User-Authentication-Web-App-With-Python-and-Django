5247bdf199747f979fcf5f9abf19395aadea487a
SamourM <48651399+SamourM@users.noreply.github.com>
2019-07-14 11:22:24 +0300

    Update urls.py

--- authenticate/urls.py
+++ authenticate/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('register/',views.register_user, name='register'),
     path('edit_profile/',views.edit_profile, name='edit_profile'),
     path('change_password/',views.change_password, name='change_password'),
-    
+    path('About_Us/',views.About_Us, name= 'About_Us'),
+    path('Services/',views.Services, name='Services'),
+    path('Contact_Us/',views.Contact_Us, name='Contact_Us'),
 ]
